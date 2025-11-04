eb37dbc5df23781dd74357bbabb9ba299defb8cf
عبير <alqaws151@gmail.com>
2025-11-04 13:22:40 +0300

    human_animal_plant.py

--- games/human_animal_plant.py
+++ games/human_animal_plant.py
@@ -1,2 +1,11 @@
-def human_animal_plant():
-    return 'Human-Animal-Plant game placeholder'
+# games/human_animal_plant.py
+import random
+
+questions = [
+    "اسم إنسان يبدأ بحرف الألف",
+    "اسم حيوان يبدأ بحرف الباء",
+    "اسم نبات يبدأ بحرف التاء"
+]
+
+def hap_question():
+    return random.choice(questions)
