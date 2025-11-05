ee7537480bfda0010656a040fc540458306de13c
عبير <alqaws151@gmail.com>
2025-11-05 21:25:36 +0300

    __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,24 +1,18 @@
 from .fast_typing import FastGame
 from .human_animal_plant import CategoryGame
 from .letters_words import WordsGame
-from .proverbs import Proverbs
 from .questions import AnalysisGame, CompatibilityGame, TruthGame
-from .reversed_word import ReversedWord
-from .mirrored_words import MirroredWords
-from .iq_questions import IQGame
-from .scramble_word import ArrangeGame
 from .chain_words import GuessGame
+from .scramble_word import ArrangeGame
+from .iq_questions import IQGame
 
 __all__ = [
     'FastGame',
     'CategoryGame',
     'WordsGame',
-    'Proverbs',
     'AnalysisGame',
     'CompatibilityGame',
     'TruthGame',
-    'ReversedWord',
-    'MirroredWords',
     'IQGame',
     'ArrangeGame',
     'GuessGame'
