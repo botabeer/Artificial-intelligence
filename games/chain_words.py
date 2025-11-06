98c50500ad67f5ff27589bb4ba3ba077aa367cad
عبير <alqaws151@gmail.com>
2025-11-06 07:02:50 +0300

    chain_words.py

--- games/chain_words.py
+++ games/chain_words.py
@@ -1,13 +1,36 @@
-class ChainWords:
-    WORDS = ["مدرسة", "قلم", "كتاب", "حاسوب", "نافذة", "حديقة"]
-
-    def start(self, last_letter=None):
-        if last_letter:
-            options = [w for w in self.WORDS if w.startswith(last_letter)]
-            word = options[0] if options else self.WORDS[0]
-        else:
-            word = self.WORDS[0]
-        return {"word": word, "emoji": "🔗"}
-
-    def check_answer(self, data, user_input):
-        return user_input.strip() == data['word']
+class GuessGame:
+    def __init__(self, gemini_helper):
+        self.gemini_helper = gemini_helper
+        self.current_hint = None
+        self.current_answer = None
+        self.category = None
+        self.tries_left = 3
+    
+    def generate_question(self):
+        """توليد سؤال تخمين"""
+        data = self.gemini_helper.generate_guess_question()
+        self.current_hint = data['hint']
+        self.current_answer = data['answer']
+        self.category = data['category']
+        
+        return f"🤔 خمن:\n\n{self.current_hint}\n\n💡 لديك {self.tries_left} محاولات"
+    
+    def check_answer(self, user_answer):
+        """التحقق من الإجابة"""
+        user_answer = user_answer.strip()
+        
+        # مطابقة مباشرة
+        if user_answer == self.current_answer:
+            return True
+        
+        # استخدام Gemini للتحقق
+        return self.gemini_helper.check_answer_similarity(user_answer, self.current_answer)
+    
+    def get_correct_answer(self):
+        """الحصول على الإجابة الصحيحة"""
+        return self.current_answer
+    
+    def decrement_tries(self):
+        """تقليل عدد المحاولات"""
+        self.tries_left -= 1
+        return self.tries_left
