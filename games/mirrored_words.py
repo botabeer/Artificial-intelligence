0f2c37ea2e0b9b91f835e88c82b93c70269e5866
عبير <alqaws151@gmail.com>
2025-11-07 17:58:49 +0300

    Delete games/mirrored_words.py

--- games/mirrored_words.py
+++
@@ -1,9 +0,0 @@
-class MirroredWords:
-    WORDS = ["باب", "مدرسة", "قلم", "حاسوب", "كتاب"]
-
-    def start(self):
-        word = random.choice(self.WORDS)
-        return {"word": word, "emoji": "↔️"}
-
-    def check_answer(self, data, user_input):
-        return user_input.strip() == data['word'][::-1]
