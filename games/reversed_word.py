4580c919563df88b219df312909dfffdc1432ed3
عبير <alqaws151@gmail.com>
2025-11-07 17:59:10 +0300

    Delete games/reversed_word.py

--- games/reversed_word.py
+++
@@ -1,6 +0,0 @@
-class ReversedWord:
-    def start(self):
-        return {"question": "اقلب الكلمة: مرحبا", "emoji": "↔️"}
-
-    def check_answer(self, data, answer):
-        return answer.strip() == "ابحرم"
